4f307ba9cb836331e35e9fe2f50937e263524278
Mat911 <matisse.demontis@gmail.com>
2025-06-27 09:32:51 +0400

    Create Version20250627053155.php

---
+++ migrations/Version20250627053155.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250627053155 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP is_fleuriste');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `user` ADD is_fleuriste TINYINT(1) DEFAULT NULL');
+    }
+}
